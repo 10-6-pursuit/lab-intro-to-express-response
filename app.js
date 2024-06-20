678c65e4113019116e2c9d3d0074b94fd991965a
ayoublos <abelibel@pursuit.org>
2024-06-20 00:00:09 -0700

    finished mandatory part of lab

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const{show}=require(`./src/quoteController`)
 const quotes=require(`./data/famousQuotes.json`)
+const {magic8Responses}=require(`./data/magic8Data`)
 
+
+app.get("/magic8", (request, response) => { 
+   let randomNumber=Math.floor(Math.random()*magic8Responses.length)
+   console.log(magic8Responses)
+    response.send(`<h1>${magic8Responses[randomNumber]}</h1>`);
+})
 app.get("/:id", (request, response) => { 
 
     const id=request.params.id
     response.send(show(quotes,id));
 })
+
 
 
 app.listen(3000, () => {
--- src/quoteController.js
+++ src/quoteController.js
@@ -1,7 +1,37 @@
 function show(quotes,id){
-    let character=quotes.find(quote=>quote.name==id)
+   switch(id){
+    case 'scareface':
+       return `say hello to my little friend`;
+       break;
+      case `homersimpson`:
+         return ` D'Oh`;
+         break 
+      case `Batman`:
+         return `To the Batmobile!`;
+         break 
+      case `Fraiser`:
+         return ` I'm listening`;
+         break 
+      case `Frankenstein`:
+         return `It's alive! It's alive`;
+         break 
+      case `Rocky`:
+         return `Yo, Adrian`;
+         break 
+      case `Gollum`:
+         return `My precious`;
+         break 
+      case `homersimpson`:
+         return ` D'Oh`;
+         break 
+      case `homersimpson`:
+         return ` D'Oh`;
+         break 
+      case `homersimpson`:
+         return ` D'Oh`;
+         break 
 
-    return character.quote
+   }
 
 
 }
