656b65e9000e8a9f86e9a1792ac9f0a87a222319
kurtCoding <kjulien7790@gmail.com>
2024-06-22 16:00:09 -0400

    added magic 8 response

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
+const magic8Responses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes - Definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes, and signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+];
+
 app.get("/", (req, res) => {
     res.send("Hello World!")
 });
@@ -47,6 +70,10 @@ app.get("/Tony_Montana", (req, res) => {
 app.get("/Jack-Dawson", (req, res) => {
     res.send("Im king of the world!")
 });
+
+app.get("/magic8", (req, res) => {
+    res.send(`<h1>${magic8Responses}</h1>`)
+})
 
 app.listen(PORT,() => {
     console.log(`listening on port ${PORT}`)
