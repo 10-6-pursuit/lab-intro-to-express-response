8677384a84b21944cfa915f04bba5b70574f7879
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-06-23 10:54:44 -0400

    added .env file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require("./app.js");
-const PORT = 3003;
+require("dotenv").config();
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
